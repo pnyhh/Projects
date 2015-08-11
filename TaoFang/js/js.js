44318ae5abd99d5d1c4bd0b0c9b30503597eab14
pnyhh <20379947@qq.com>
2015-08-12 00:21:19 +0800

    TaoFang update8.12

--- TaoFang/js/js.js
+++ TaoFang/js/js.js
@@ -4,8 +4,8 @@ $(function() {
 	var index = 0;
 	var picTimer;
 	var wWidth = document.documentElement.clientWidth;
-	$("#focus ul li").css("width",wWidth);
-	$("#focus ul li a img").css({"width":wWidth,"height":600});
+	$("#focus ul li").css("width",1280);
+	$("#focus ul li a img").css({"width":1280,"height":600});
 	//以下代码添加数字按钮和按钮后的半透明条，还有上一页、下一页两个按钮
 	var btn = "<div class='btn'>";
 	for(var i=0; i < len; i++) {
@@ -42,7 +42,7 @@ $(function() {
 	});
 
 	//本例为左右滚动，即所有li元素都是在同一排向左浮动，所以这里需要计算出外围ul元素的宽度
-	$("#focus ul").css("width",sWidth * (len));
+	$("#focus ul").css("width",1280 * (len));
 	
 	//鼠标滑上焦点图时停止自动播放，滑出时开始自动播放
 	$("#focus").hover(function() {
@@ -57,9 +57,15 @@ $(function() {
 	
 	//显示图片函数，根据接收的index值显示相应的内容
 	function showPics(index) { //普通切换
-		var nowLeft = -index*sWidth; //根据index值计算ul元素的left值
+		var nowLeft = -index*1280; //根据index值计算ul元素的left值
 		$("#focus ul").stop(true,false).animate({"left":nowLeft},300); //通过animate()调整ul元素滚动到计算出的position
 		//$("#focus .btn span").removeClass("on").eq(index).addClass("on"); //为当前的按钮切换到选中的效果
 		$("#focus .btn span").stop(true,false).animate({"opacity":"0.4"},300).eq(index).stop(true,false).animate({"opacity":"1"},300); //为当前的按钮切换到选中的效果
 	}
 });
+$(window).scroll( function(){
+	var top= document.body.scrollTop || document.documentElement.scrollTop;
+	if(top > 1334){
+		$("#mask").fadeIn(3000);
+	}
+})
