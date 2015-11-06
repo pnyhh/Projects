fb84dc3a2e0282f0f1619c07ddcbee4b6e7b22b6
pnyhh <20379947@qq.com>
2015-11-06 16:29:07 +0800

    WxPage3 update 20151106 v0.00 with wechat jssdk

---
+++ WeChat/wxpage3/js/check.js
@@ -0,0 +1,51 @@
+﻿var createNonceStr = function () {
+    return Math.random().toString(36).substr(2, 15);
+};
+var createTimestamp = function () {
+    return parseInt(new Date().getTime() / 1000) + '';
+};
+var raw = function (args) {
+    var keys = Object.keys(args);
+    keys = keys.sort()
+    var newArgs = {};
+    keys.forEach(function (key) {
+        newArgs[key.toLowerCase()] = args[key];
+    });
+    var string = '';
+    for (var k in newArgs) {
+        string += '&' + k + '=' + newArgs[k];
+    }
+    string = string.substr(1);
+    return string;
+};
+var sign = function (jsapi_ticket, url) {
+    var ret = {
+        jsapi_ticket: jsapi_ticket,
+        nonceStr: createNonceStr(),
+        timestamp: createTimestamp(),
+        url: url
+    };
+    var string = raw(ret);
+    shaObj = new jsSHA(string, 'TEXT');
+    ret.signature = shaObj.getHash('SHA-1', 'HEX');
+    return ret;
+};
+function runfun(fun){
+	setTimeout(fun, 2000);
+}
+function runajax (){
+    $.ajax({
+		type:"get",
+		url:"weixin.php",
+		async:true,
+		dataType:"json",
+		success:function(data){
+			console.log(data.ticket);
+			set_ticket(data.ticket);
+		}
+	});
+}
+function set_ticket (data) {
+    localStorage.setItem("ticket", data);
+    window.location.reload();
+}
--- WeChat/wxpage3/js/js.js
+++ WeChat/wxpage3/js/js.js
@@ -13,28 +13,27 @@ window.onload = function(){
 		"img/slider13.png",
 		"img/slider14.png",
 		"img/slider21.png",
-		"img/slider31.png",
+		"img/slider34.png",
+		"img/slider32.png",
+		"img/slider33.png",
+		"img/slider35.png",
+		"img/slider51.png",
+		"img/qy-logo-1.jpg",
+		"img/qy-logo-7.jpg",
+		"img/qy-logo-9.jpg",
+		"img/qy-logo-4.jpg",
+		"img/qy-logo-5.jpg",
+		"img/qy-logo-6.jpg",
+		"img/qy-logo-2.jpg",
+		"img/qy-logo-8.jpg",
+		"img/qy-logo-3.jpg",
+		"img/qy-logo-10.jpg",
 		"img/slider41.png",
 		"img/slider42.png",
 		"img/slider41.png",
 		"img/slider422.png",
 		"img/slider41.png",
 		"img/slider432.png",
-		"img/slider51.png",
-		"img/stu1.png",
-		"img/stu2.png",
-		"img/stu3.png",
-		"img/stu4.png",
-		"img/stu5.png",
-		"img/stu6.png",
-		"img/stu7.png",
-		"img/stu8.png",
-		"img/stu9.png",
-		"img/stu10.png",
-		"img/stu11.png",
-		"img/stu12.png",
-		"img/slider71.png",
-		"img/slider72.png"
 	];
 	var imgs = wrap.getElementsByTagName("img");
 	var tImgs = [];
@@ -52,8 +51,12 @@ window.onload = function(){
 	var Ifq = document.getElementById("Ifq");
 	var Idyf = document.getElementById("Idyf");
 	var Ilt = document.getElementById("Ilt");
+	var bl = H/736;
 		slider11.style.width = W*0.8+"px";
 		slider11.parentNode.style.left = W/2 - slider11.width/2 + "px";
+		Ifq.style.height = 245*bl + "px";
+		Idyf.style.height = 245*bl + "px";
+		Ilt.style.height = 245*bl + "px";
 		Ifq.parentNode.style.left = W/2 - Ifq.width/2 + "px";
 		Idyf.parentNode.style.left = W/2 - Ifq.width/2 + "px";
 		Ilt.parentNode.style.left = W/2 - Ifq.width/2 + "px";
